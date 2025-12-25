8f2b7a7675503bf101b85ad4c23f5d94a8adf66d
Meski Abdelilah <meskiabdelilah10@gmail.com>
2025-12-25 15:14:50 +0100

    feat(models): compte baseModel with database

--- app/models/baseModel.php
+++ app/models/baseModel.php
@@ -1,9 +1,16 @@
 <!-- كل الكلاسات غادي تورث منو -->\
 <?php
+require_once  '../config/database.php';
+
 abstract class baseModel{
 
+    protected $db;
+    public function __construct()
+    {
+        $this-> db = database::getInstance()->getConnection();
+    }
     abstract public function save();
     abstract public function delete();
-    abstract public function find($id);
-    abstract public function all();
+    abstract public  static function find($id);
+    abstract public static function all();
 }
