7b1d04362498d91a8cf74fe2b0749697ccd42365
Meski Abdelilah <meskiabdelilah10@gmail.com>
2025-12-25 15:09:40 +0100

    feat(config): add database class

---
+++ app/config/database.php
@@ -0,0 +1,31 @@
+ <!-- الاتصال بـ PDO -->
+ <?php
+
+    class  database
+    {
+        private static $Instance = null;
+        private PDO $pdo;
+        private function __construct()
+        {
+            try {
+                $this->pdo = new PDO('mysql:host=huey;dbname=app_bancaire', 'root', '');
+                $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            } catch (PDOException $err) {
+                die($err->getMessage());
+            }
+        }
+
+        public static function getInstance()
+        {
+            if (self::$Instance === null) {
+                self::$Instance = new database();
+            }
+            return self::$Instance;
+        }
+
+        public function getConnection()
+        {
+            return $this->pdo;
+        }
+    }
