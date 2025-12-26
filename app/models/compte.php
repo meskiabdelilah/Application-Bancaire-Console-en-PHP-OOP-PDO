64e038ef336628eeead9fd0a5394a9409dd1ac1a
Meski Abdelilah <meskiabdelilah10@gmail.com>
2025-12-26 14:50:56 +0100

    feat(models): add protected type

--- app/models/compte.php
+++ app/models/compte.php
@@ -4,9 +4,11 @@ abstract class compte extends baseModel {
     protected $numero;
     protected $solde;
     protected $clients_id;
+    protected $type;
 
     public function __construct($numero, $solde, $clients_id)
     {
+        parent::__construct();
         $this->numero = $numero;
         $this->solde = $solde;
         $this->clients_id = $clients_id;
